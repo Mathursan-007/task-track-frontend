6568b24f0dcde2e27c6bf50420e64c9d980443ce
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-27 16:31:57 +0530

    file commit

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -9,7 +9,14 @@ class MainContainer extends React.Component{
 
     state={
         cards:[],
-        lists:[]
+        lists:[],
+        File:''
+    }
+
+    fileHandler=(e)=>{
+
+         this.setState({File:e.target.files[0]})
+
     }
 
     createNewCard=(input)=>{
@@ -36,37 +43,52 @@ class MainContainer extends React.Component{
     }
 
 
-    addList=(cardID,input)=>{
-        fetch('https://task-track-backend.herokuapp.com/card/lists',{
-            method:'POST',
-            headers:{
+    addList=(cardID,input)=> {
+        fetch('https://task-track-backend.herokuapp.com/card/lists', {
+            method: 'POST',
+            headers: {
 
-                'Content-type':'application/json',
-                Accept:'application/json'
+                'Content-type': 'application/json',
+                Accept: 'application/json'
             },
-            body:JSON.stringify({
-                description:input,
-                card_ID:cardID,
-                completed:false
+            body: JSON.stringify({
+                description: input,
+                card_ID: cardID,
+                completed: false
             })
         })
-            .then(resp=>resp.json())
-            .then(newList=>{
-                const foundCard ={...this.state.cards.find(card=>card.id==cardID)}
-                 foundCard.lists=[...foundCard.lists,newList]
+            .then(resp => resp.json())
+            .then(newList => {
+                const foundCard = {...this.state.cards.find(card => card.id == cardID)}
+                foundCard.lists = [...foundCard.lists, newList]
 
-                const newCards = this.state.cards.map(card=>{
-                    if(card.id==cardID){
+                const newCards = this.state.cards.map(card => {
+                    if (card.id == cardID) {
                         return foundCard;
-                    }else{
+                    } else {
                         return card;
                     }
                 })
 
                 this.setState({
-                    cards:newCards
+                    cards: newCards
                 })
             })
+
+        const fd= new FormData();
+        fd.append("File",this.state.File)
+
+        fetch('https://task-track-backend.herokuapp.com/card/add', {
+            method: 'POST',
+            headers: {
+
+                'Content-type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                fd
+            })
+        }).then(res => console.log(res))
 
     }
 
@@ -141,7 +163,14 @@ class MainContainer extends React.Component{
     render() {
         return(
             <div className="main-container">
-                    <ToDoCardContainer cards={this.state.cards} addList={this.addList} handleClickList={this.handleClickList}/>
+                <input
+                    type="file"
+                    accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document,
+                             application/vnd.openxmlformats-officedocument.presentationml.presentation"
+                    name="File"
+                    onChange={this.fileHandler}
+                />
+                <ToDoCardContainer cards={this.state.cards} addList={this.addList} handleClickList={this.handleClickList}/>
                     <CreateCard createNewCard ={this.createNewCard}/>
             </div>
         )
